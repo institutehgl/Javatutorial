fb9d1e2d7021449cd2942117a02c93966282f831
sir <simise3753@j24blog.com>
2021-02-18 09:31:41 +0530

    initial commit

---
+++ src/Intro.java
@@ -0,0 +1,13 @@
+public class Intro {
+    public static void main(String[] args) {
+        //single line comment
+        /*
+        multi
+                line
+                        COMMENT
+         */
+
+        //prints raja
+        System.out.println("raja");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        //variable
+        // A variable is a named memory location which holds some data
+        // there are two types of variables are there
+        // primitive variables
+        // boolean,byte,short,char,int,float,double,long
+        // compound variables
+        //strings,arrays,classes,interfaces,enums
+        //syntax for variable  declaration
+        //data-type variable-name;
+        //variable initialization
+        //varaiable-name = value of that type
+        //price of type integer
+        int price;
+        //initialization of price
+        price=1000;
+        System.out.println(price);
+        byte value = 127;
+        System.out.println(value);
+        char letterA = 'A';
+        System.out.println(letterA);
+        short price1;
+        price1=1000;
+        System.out.println(price1);
+        float value1 = 17.0f;
+        System.out.println(value1);
+        double db1 = 2.25151551d;
+        System.out.println(db1);
+        boolean price2;
+        price2=true;
+        System.out.println(price2);
+
+
+
+    }
+
+
+}
