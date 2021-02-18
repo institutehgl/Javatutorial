d38cd0e9c0b9a6924bd3a8a21e717e61b84789da
sir <simise3753@j24blog.com>
2021-02-18 10:12:34 +0530

    initial

--- src/Compoundstypes.java
+++ src/Compoundstypes.java
@@ -1,11 +1,12 @@
 public class Compoundstypes implements Myinterface {
 
-    String s ;
+    String s;
 
-    Compoundstypes(){
+    Compoundstypes() {
         s = "hello world!";
     }
-    void greetings(){
+
+    void greetings() {
         System.out.println(s);
     }
 
@@ -18,33 +19,41 @@ public class Compoundstypes implements Myinterface {
         //datatype[] variable-name = {value1,value2};
 
         //1d array
-        int[] array1 ={1,2,3};
+        char[] array1 = {'a','b', 'c'};
         System.out.println(array1[0]);
         System.out.println(array1[1]);
         System.out.println(array1[2]);
 
         //2d array
-        int[][] array2 ={{1,2},{1,2}};
-        System.out.println(array2[0][0]);
+        int[][] array2 = {{1, 2},
+                          {1, 2}};
+
+        System.out.print(array2[0][0]+" ");
         System.out.println(array2[0][1]);
-        System.out.println(array2[1][0]);
+        System.out.print(array2[1][0]+" ");
         System.out.println(array2[1][1]);
 
         //strings
-        String st = "hello";
+        String st = "hello ";
+        String raj="world";
+        String combined= st+raj;
         System.out.println(st);
         //helper functions
-        System.out.println(st.toUpperCase());
+        System.out.println(st.charAt(4));
         System.out.println(st.length());
+        System.out.println(st.trim());
+        System.out.println(combined);
+
 
         //enums
-        Level  level = Level.HIGH;
+        Level level = Level.HIGH;
         System.out.println(level);
 
         Compoundstypes c = new Compoundstypes();
         c.greets();
 
     }
+
     public void greets() {
         System.out.println("Myinterface function");
     }
--- src/Operators.java
+++ src/Operators.java
@@ -1,7 +1,12 @@
 public class Operators {
     public static void main(String[] args){
 
-        System.out.println("Hi");
+        //operators
+
+
+
+
+
 
     }
 }
